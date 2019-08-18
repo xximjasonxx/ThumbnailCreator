7ed0e47ced116be5bd90c37c78413b57ab66f4eb
Jason Farrell <imjason@gmail.com>
2019-08-18 18:26:04 -0500

    Added Lambda

---
+++ CreateThumbnailFunction/src/CreateThumbnailFunction/CreateThumbnailFunction.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Amazon.S3.Util;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace CreateThumbnailFunctions
+{
+    public class CreateThumbnailFunction
+    {
+        /// <summary>
+        /// This method is called for every Lambda invocation. This method takes in an S3 event object and can be used 
+        /// to respond to S3 notifications.
+        /// </summary>
+        /// <param name="evnt"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<string> Execute(S3Event evnt, ILambdaContext context)
+        {
+            var s3Event = evnt.Records?[0].S3;
+            if (s3Event == null)
+            {
+                return "No Event";
+            }
+
+            return $"Event Received - {s3Event.Object.Key}";
+        }
+    }
+}
