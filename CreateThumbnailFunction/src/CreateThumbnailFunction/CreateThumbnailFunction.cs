3075076f06e7903cdad4f063835c43248e5e67b2
Jason Farrell <imjason@gmail.com>
2019-08-24 08:57:31 -0500

    Removed unecessary assemblies

--- CreateThumbnailFunction/src/CreateThumbnailFunction/CreateThumbnailFunction.cs
+++ CreateThumbnailFunction/src/CreateThumbnailFunction/CreateThumbnailFunction.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
-using Amazon.S3;
-using Amazon.S3.Util;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
