819c08559ea59055ccf2e16d105deeaa6d4f20ba
Jason Farrell <imjason@gmail.com>
2019-08-25 18:03:39 -0500

    Added logic to create the thumbnail and save it

--- CreateThumbnailFunction/src/CreateThumbnailFunction/CreateThumbnailFunction.cs
+++ CreateThumbnailFunction/src/CreateThumbnailFunction/CreateThumbnailFunction.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Drawing;
+using System.IO;
 using System.Threading.Tasks;
-
+using Amazon;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -18,17 +22,49 @@ namespace Functions
         /// <param name="evnt"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public async Task<string> ExecuteAsync(S3Event evnt, ILambdaContext context)
+        public async Task ExecuteAsync(S3Event evnt, ILambdaContext context)
         {
-            var s3Event = evnt.Records?[0].S3;
-            context.Logger.Log("Invoking Function");
-            /*if (s3Event == null)
+            try
             {
-                return "No Event";
-            }
+                context.Logger.Log("Invoking Function");
 
-            return $"Event Received - {s3Event.Object.Key}";*/
-            return "Executed";
+                var s3Event = evnt.Records?[0].S3;
+                var getRequest = new GetObjectRequest
+                {
+                    BucketName = s3Event.Bucket.Name,
+                    Key = s3Event.Object.Key
+                };
+
+                using (var client = new AmazonS3Client(RegionEndpoint.USEast1))
+                {
+                    using (var getResponse = await client.GetObjectAsync(getRequest))
+                    {
+                        using (var rawImage = Image.FromStream(getResponse.ResponseStream))
+                        {
+                            context.Logger.LogLine("Got image - creating thumbnail");
+                            var thumbnail = rawImage.GetThumbnailImage(120, 120, () => false, IntPtr.Zero);
+                            thumbnail.Save("thumbnail");
+                            context.Logger.LogLine("Save Image to file");
+
+                            // put the thumbnail
+                            var putRequest = new PutObjectRequest
+                            {
+                                BucketName = "thumbimagestc1983",
+                                Key = s3Event.Object.Key,
+                                FilePath = "thumbnail"
+                            };
+
+                            var putResponse = await client.PutObjectAsync(putRequest);
+                            context.Logger.LogLine("Write Complete");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                context.Logger.LogLine(ex.Message);
+                context.Logger.LogLine(ex.StackTrace);
+            }
         }
     }
 }
