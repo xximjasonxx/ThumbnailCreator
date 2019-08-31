a62a4243357e7f56528c090e71519da60f38da57
Jason Farrell <imjason@gmail.com>
2019-08-31 12:30:41 -0500

    Added Code to query labels for image

--- AnalyzeImageFunction/src/AnalyzeImageFunction/AnalyzeImageFunction.cs
+++ AnalyzeImageFunction/src/AnalyzeImageFunction/AnalyzeImageFunction.cs
@@ -1,8 +1,14 @@
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
-
+using Amazon;
 using Amazon.Lambda.Core;
-using Amazon.Lambda.S3Events;
+using Amazon.Lambda.SNSEvents;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using AnalyzeImageFunction;
+using Newtonsoft.Json;
+using System.Linq;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -19,12 +25,44 @@ namespace Functions
         /// <param name="evnt"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public async Task<string> ExecuteAsync(S3Event evnt, ILambdaContext context)
+        public async Task ExecuteAsync(SNSEvent evnt, ILambdaContext context)
         {
-            //var s3Event = evnt.Records?[0].S3;
-            context.Logger.LogLine("Function fired");
+            var snsData = JsonConvert.DeserializeObject<SnsRecord>(evnt.Records?[0].Sns.Message);
+            var s3Data = snsData.Records.ElementAt(0).S3;
+            using (var client = new AmazonRekognitionClient(RegionEndpoint.USEast1))
+            {
+                var detectedLabels = await DetectLabelsAsync(client,
+                    s3Data.Bucket.Name,
+                    s3Data.Object.Key);
 
-            return string.Empty;
+                foreach (Label label in detectedLabels)
+                    context.Logger.LogLine($"{label.Name}: {label.Confidence}");
+            }
+        }
+
+        async Task<IList<Label>> DetectLabelsAsync(AmazonRekognitionClient client, string bucketName, string keyName)
+        {
+            var labelsRequest = new DetectLabelsRequest
+            {
+                Image = new Image
+                {
+                    S3Object = new S3Object()
+                    {
+                        Name = keyName,
+                        Bucket = bucketName
+                    }
+                },
+                MaxLabels = 10,
+                MinConfidence = 75f
+            };
+
+            var response = await client.DetectLabelsAsync(labelsRequest);
+            return response.Labels;
+        }
+
+        async Task WriteToDynamoTableAsync()
+        {
+
         }
     }
 }
