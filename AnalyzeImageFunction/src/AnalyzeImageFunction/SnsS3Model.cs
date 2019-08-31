2d46d681eeee4303a1fa9f81c27a5d1416486d98
Jason Farrell <imjason@gmail.com>
2019-08-30 22:33:36 -0500

    Added SnsS3Model to Analyze function

---
+++ AnalyzeImageFunction/src/AnalyzeImageFunction/SnsS3Model.cs
@@ -0,0 +1,31 @@
+
+using System.Collections.Generic;
+
+namespace AnalyzeImageFunction
+{
+    public class SnsRecord
+    {
+        public ICollection<SnsS3Model> Records { get; set; }
+    }
+
+    public class SnsS3Model
+    {
+        public S3Model S3 { get; set; }
+    }
+
+    public class S3Model
+    {
+        public S3BucketModel Bucket { get; set; }
+        public S3ObjectModel Object { get; set; }
+    }
+
+    public class S3BucketModel
+    {
+        public string Name { get; set; }
+    }
+
+    public class S3ObjectModel
+    {
+        public string Key { get; set; }
+    }
+}
